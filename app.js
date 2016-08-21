14e2e6fb059f0efeed9d76f544d9df07bf582945
Kaylee Alvarado <kaymay1000@gmail.com>
2016-08-21 10:12:37 -0700

    Hourly bean data table complete. Next task: hourly employee data table.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm'];
 var numCupsPerPound = 16;
 var coffeeShopsArray = [];
-console.log(coffeeShopsArray);
 
 function round(num, places) { //Rounding helper function
   return parseFloat(num.toFixed(places));
@@ -74,7 +73,7 @@ CoffeeShop.prototype.generateEmployeesPerHour = function() {
   this.employeesPerHour.push(tempEmployees);
 };
 
-CoffeeShop.prototype.render = function() {
+CoffeeShop.prototype.generateShopInformation = function() {
   this.getRandomNum(this.minNumCust, this.maxNumCust);
   this.generateCustomersPerHour();
   this.generateCupsPerHour();
@@ -85,15 +84,16 @@ CoffeeShop.prototype.render = function() {
   this.generateEmployeesPerHour();
 };
 
-var pikePlaceMarket = new CoffeeShop('Pike Place Market', 14, 35, 1.2, 0.34);
-var capitolHill = new CoffeeShop('Capitol Hill', 12, 28, 3.2, 0.03);
-var seattlePublicLibrary = new CoffeeShop('Seattle Public Library', 9, 45, 2.6, 0.02);
-var southLakeUnion = new CoffeeShop('South Lake Union', 5, 18, 1.3, 0.04);
-var seaTacAirport = new CoffeeShop('Sea-Tac Airport', 28, 44, 1.1, 0.41);
+pikePlaceMarket = new CoffeeShop('Pike Place Market', 14, 35, 1.2, 0.34);
+capitolHill = new CoffeeShop('Capitol Hill', 12, 28, 3.2, 0.03);
+seattlePublicLibrary = new CoffeeShop('Seattle Public Library', 9, 45, 2.6, 0.02);
+southLakeUnion = new CoffeeShop('South Lake Union', 5, 18, 1.3, 0.04);
+seaTacAirport = new CoffeeShop('Sea-Tac Airport', 28, 44, 1.1, 0.41);
+console.log(coffeeShopsArray);
 
 var renderCoffeeShops = function (){
   for (var i = 0; i < coffeeShopsArray.length; i++) {
-    coffeeShopsArray[i].render();
+    coffeeShopsArray[i].generateShopInformation();
   }
 };
 renderCoffeeShops();
@@ -104,22 +104,24 @@ function generateAllShopsDailyTotal() {
   for (var i = 0; i < coffeeShopsArray.length; i++){
     var tempAllShopsTotalDailyPoundsNeeded = coffeeShopsArray[i].totalDailyPoundsNeeded;
     allShopsTotalDailyPoundsNeeded += tempAllShopsTotalDailyPoundsNeeded;
-    console.log(allShopsTotalDailyPoundsNeeded);
   }
 }
 
-// var allShopsTotalHourlyPoundsNeededArray = [];
-// var allShopsTotalHourlyPoundsNeeded = 0;
-//
-// function generateAllShopsHourlyTotals() {
-//   for (var i = 0; i < hours.length; i++) {
-//     for (j = 0; i < coffeeShopsArray.length; j++) {
-//       var tempTotalHourlyPoundsAllLocations = coffeeShopsArray[i].totalHourlyPoundsNeeded;
-//       allShopsTotalHourlyPoundsNeeded += tempTotalHourlyPoundsAllLocations;
-//     }
-//     allShopsTotalHourlyPoundsNeededArray.push(allShopsTotalHourlyPoundsNeeded);
-//   }
-// }
+var allShopsTotalHourlyPoundsNeededArray = [];
+
+function generateAllShopsHourlyTotals() {
+  for (i = 0; i < hours.length; i++) {
+    var allShopsTotalHourlyPoundsNeeded = 0;
+    for (var j = 0; j < coffeeShopsArray.length; j++) {
+      var tempTotalHourlyPoundsAllLocations = coffeeShopsArray[j].totalHourlyPounds[i];
+      console.log(tempTotalHourlyPoundsAllLocations + ' add me');
+      allShopsTotalHourlyPoundsNeeded += tempTotalHourlyPoundsAllLocations;
+      console.log(allShopsTotalHourlyPoundsNeeded + ' total');
+    }
+    allShopsTotalHourlyPoundsNeededArray.push(round(allShopsTotalHourlyPoundsNeeded, 1));
+    console.log(allShopsTotalHourlyPoundsNeededArray);
+  }
+}
 
 //DOM manipulation
 
@@ -128,11 +130,13 @@ var tableEl = document.getElementById('shop-data-table'); //hook to hard-coded t
 function generateTableHeader() {
   var trEl = document.createElement('tr');
   var emptyThEl = document.createElement('th'); //creates empty cell in top left corner of table
-  emptyThEl.textContent = '';
   var dailyLocationTotalThEl = document.createElement('th');
+
+  emptyThEl.textContent = '';
   dailyLocationTotalThEl.textContent = 'Daily Location Total';
   trEl.appendChild(emptyThEl);
   trEl.appendChild(dailyLocationTotalThEl);
+
   for (var i in hours) { //creates table header elements that contain hours[i] in each cell
     var thEl = document.createElement('th');
     thEl.textContent = hours[i];
@@ -144,11 +148,13 @@ function generateTableBody() {
   for (var i = 0; i < coffeeShopsArray.length; i++) {
     var coffeeShopNameTrEl = document.createElement('tr');
     var coffeeShopNameTdEl = document.createElement('td'); //lists names of coffee shops on far left of table
+    var totalDailyPoundsTdEl = document.createElement('td');
+
     coffeeShopNameTdEl.textContent = coffeeShopsArray[i].name;
     coffeeShopNameTrEl.appendChild(coffeeShopNameTdEl);
-    var totalDailyPoundsTdEl = document.createElement('td');
     totalDailyPoundsTdEl.textContent = coffeeShopsArray[i].totalDailyPoundsNeeded;//grabs the Coffee Shop object at i in the coffeeShopsArray and gets that Coffee Shop's totalDailyPoundsNeeded value
     coffeeShopNameTrEl.appendChild(totalDailyPoundsTdEl);
+
     for (var j = 0; j < hours.length; j++) {
       var totalHourlyPoundsTdEl = document.createElement('td');
       totalHourlyPoundsTdEl.textContent = coffeeShopsArray[i].totalHourlyPounds[j]; //grabs the Coffee Shop object at i in the coffeeShopsArray and gets that Coffee Shop's totalHourlyPounds array at j (its totalHourlyPoundsNeeded value)
@@ -157,17 +163,22 @@ function generateTableBody() {
     tableEl.appendChild(coffeeShopNameTrEl);
   }
   var totalsTrEl = document.createElement('tr');
-  totalsTrEl.textContent = 'Totals';
   var allShopsTotalDailyPoundsTdEl = document.createElement('td');
+
+  totalsTrEl.textContent = 'Totals';
   allShopsTotalDailyPoundsTdEl.textContent = allShopsTotalDailyPoundsNeeded;
   totalsTrEl.appendChild(allShopsTotalDailyPoundsTdEl);
-  // var allShopsTotalHourlyPoundsTdEl = document.createElement('td');
-  // allShopsTotalHourlyPoundsTdEl.textContent = allShopsTotalHourlyPoundsNeededArray[i];
-  // totalsTrEl.appendChild(allShopsTotalHourlyPoundsTdEl);
+
+  for (var k = 0; k < hours.length; k++) {
+    var allShopsTotalHourlyPoundsTdEl = document.createElement('td');
+    allShopsTotalHourlyPoundsTdEl.textContent = allShopsTotalHourlyPoundsNeededArray[k];
+    totalsTrEl.appendChild(allShopsTotalHourlyPoundsTdEl);
+  }
+
   tableEl.appendChild(totalsTrEl);
 };
 
 generateAllShopsDailyTotal();
-//generateAllShopsHourlyTotals();
+generateAllShopsHourlyTotals();
 generateTableHeader();
 generateTableBody();
